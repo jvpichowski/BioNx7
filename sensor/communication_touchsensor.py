fd06d094ca743740cbf3026eb4466f3ae9a269f0
bene401 <31989084+bene401@users.noreply.github.com>
2021-05-23 16:59:08 +0200

    Listener for touch events

--- sensor/communication_touchsensor.py
+++ sensor/communication_touchsensor.py
@@ -8,23 +8,41 @@ if ser.isOpen():
 ser.open()
 print("connected: " + str(ser.isOpen()))
 
-
+# prototypischer Listener, der koordinaten speichert, wenn Touch-Sensor berührt wird
+points = []
 while 1:
-    userInput = input("action: ")
-    if userInput == "down":
-        ser.write(b"down")
-        print("move down")
-    if userInput == "up":
-        ser.write(b"up")
-        print("move up")
-    if userInput == "test":
-        ser.write(b"test")
-        print("test Mode")
-    if userInput == "alarm":
-        ser.write(b"alarm")
-        print("alarm Mode")
-    if userInput == "n":
+    ser.write(b"down")
+    data = ser.readline().decode("ascii")
+    # print("Point: "+data)
+    if data == "high\r\n": 
+        # [x,y,z]       
+        points.append([1,1,1])        
+        print("Point: " + data + "Memory: " + str(points))
+    # Abruch nach 10 Messungen
+    elif len(points) == 10:
         break
+ 
+
+# Nutzer kann Befehle über Terminal eingeben
+# while 1:
+#     # User Input:
+#     userInput = input("action: ")
+#     if userInput == "down":
+#         ser.write(b"down")
+#         print("move down")
+#     if userInput == "up":
+#         ser.write(b"up")
+#         print("move up")
+#     if userInput == "test":
+#         ser.write(b"test")
+#         print("test Mode")
+#     if userInput == "alarm":
+#         ser.write(b"alarm")
+#         print("alarm Mode")
+#     if userInput == "n":
+#         break
+
+
         
 ser.close()
 print("connected: " + str(ser.isOpen()))
